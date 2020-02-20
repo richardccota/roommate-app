44b6612b432051a9d253316da72a49e50b8a58e6
richardccota <35834804+richardccota@users.noreply.github.com>
2020-02-20 15:39:23 -0800

    Add files via upload
    Login Page completed

---
+++ lib/login_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:roommate_app/authenticator.dart';
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+Future _buildErrorDialog(BuildContext context, _message) {
+  return showDialog(
+    builder: (context) {
+      return AlertDialog(
+        title: Text('Error Message'),
+        content: Text(_message),
+        actions: <Widget>[
+          FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              })
+        ],
+      );
+    },
+    context: context,
+  );
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _password;
+  String _email;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Roommate App Login'),
+          backgroundColor: Colors.lightGreen,
+        ),
+        body: Container(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Text("Please login to your Account"),
+                  SizedBox(
+                      height: 90
+                  ),
+                  TextFormField(
+                    onSaved: (value) => _email = value,
+                    decoration: InputDecoration(
+                      labelText: ("Username"),
+                      icon: Icon(Icons.account_box,
+                          color: Colors.grey),
+                    ),
+                    validator: (value) => value.isEmpty ? 'Can\'t be empty' : null,
+                  ),
+                  TextFormField(
+                    onSaved: (value) => _password = value,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: ("Password"),
+                      icon: Icon(Icons.lock,
+                          color: Colors.grey),
+                    ),
+                    validator: (value) => value.isEmpty ? 'Cannot be empty' : null,
+                  ),
+                  SizedBox(
+                      height: 30
+                  ),
+                  RaisedButton(
+                      elevation: 5,
+                      color: Colors.lightGreen,
+                      child: Text("Login"),
+                      onPressed: () async {
+                        final form = _formKey.currentState;
+                        form.save();
+
+                        // Validate will return true if is valid, or false if invalid.
+                        if (form.validate()) {
+                          try {
+                            AuthResult result =
+                            await Provider.of<AuthService>(context).loginUser(
+                                email: _email, password: _password);
+                            print(result);
+                          } on AuthException catch (error) {
+                            // handle the firebase specific error
+                            return _buildErrorDialog(context, error.message);
+                          } on Exception catch (error) {
+                            // gracefully handle anything else that might happen..
+                            return _buildErrorDialog(context, error.toString());
+                          }
+                        }
+                      },
+                  ),
+                ],
+              )
+            ),
+        ),
+      ),
+    );
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:roommate_app/login_page.dart';
+import 'package:roommate_app/home_page.dart';
+import 'package:roommate_app/authenticator.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+  ChangeNotifierProvider<AuthService>(
+    child: MyApp(),
+    builder: (BuildContext context) {
+      return AuthService();
+    },
+  ),
+);
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: FutureBuilder<FirebaseUser>(
+        future: Provider.of<AuthService>(context).getUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) { //          ⇐ NEW
+          if (snapshot.connectionState == ConnectionState.done) {
+            // log error to console                                            ⇐ NEW
+            if (snapshot.error != null) {
+              print("error");
+              return Text(snapshot.error.toString());
+            }
+            // redirect to the proper page, pass the user into the
+            // `HomePage` so we can display the user email in welcome msg     ⇐ NEW
+            return snapshot.hasData ? HomePage(snapshot.data) : LoginPage();
+          } else {
+            // show loading indicator                                         ⇐ NEW
+            return LoadingCircle();
+          }
+        },
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+class LoadingCircle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: CircularProgressIndicator(),
+        alignment: Alignment(0.0, 0.0),
+      ),
+    );
+  }
+}
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -65,9 +105,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                       RaisedButton(
                         elevation: 5,
-                          child: Text(_isLoginForm ? 'Login' : 'Create Account'),
-                          onPressed: validateAndSubmit,
-                          color: Colors.lightGreen
+                          //child: Text(_isLoginForm ? 'Login' : 'Create Account'),
+                         // onPressed: validateAndSubmit,
+                          color: Colors.lightGreen,
+                          onPressed: () => {},
                       ),
                     ],
                   ),
